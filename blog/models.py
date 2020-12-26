b131eeb761079682ad260503286a09ae3e3cf6e7
Masudul Alam <malam172034@bscse.uiu.ac.bd>
2020-12-27 03:05:59 +0600

    post class added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+
+	title = models.CharField(max_length =100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default =timezone.now) #saves the posted time and can be changed later
+	author = models.ForeignKey(User, on_delete = models.CASCADE) #deletes the posts of deleted user
+
+	def __str__(self):          #this method helps to add more detail (title) in Post.objects.all() function in django shell
+		return self.title
